a19dbe323201b34cdae67bbb0639b2d5e5469e01
Macius <ciekuds@gmail.com>
2016-12-01 23:54:13 +0100

    initial upload

---
+++ WordGame/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace WordGame
+{
+    [Activity(Label = "WordGame", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            TextView aa = FindViewById<TextView>(Resource.Id.textView1);
+            aa.Text = "Dupa";
+            // Set our view from the "main" layout resource
+            
+        }
+    }
+}
+
