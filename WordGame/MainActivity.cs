ca43e4f24fdbc7cd8d2bf2db7ddef7dd122deee5
Macius <ciekuds@gmail.com>
2016-12-02 22:40:02 +0100

    feeling test

--- WordGame/MainActivity.cs
+++ WordGame/MainActivity.cs
@@ -12,7 +12,7 @@ namespace WordGame
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
             TextView aa = FindViewById<TextView>(Resource.Id.textView1);
-            aa.Text = "Dupa z trsa";
+            aa.Text = "Maruś kocham Cie";
             // Set our view from the "main" layout resource
             
         }
