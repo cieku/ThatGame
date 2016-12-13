a258827f3085cfcd298013677a4c6bf3e5a92839
Aleksandra <aleksanda.czerwa@gmail.com>
2016-12-13 20:43:16 +0100

    Removed samples; Moved log into Google Plus to LoginActivity.cs; Preparing to merge.

--- WordGame/LoginActivity.cs
+++ WordGame/LoginActivity.cs
@@ -1,19 +1,170 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
+using System;
+
+using Android.Content;
+using Android.Gms.Common;
+using Android.Gms.Common.Apis;
+using Android.Gms.Plus;
+using Android.Gms.Plus.Model.People;
+using Android.Runtime;
+using Android.OS;
+
+using static Android.Gms.Common.Apis.GoogleApiClient;
 
 namespace WordGame
 {
-    
-    class LoginActivity 
+
+    [Activity(Label = "WordGame", MainLauncher = true, Icon = "@drawable/icon")]
+    public class LoginActivity : Activity, IConnectionCallbacks, IOnConnectionFailedListener
     {
+
+        private GoogleApiClient googleApiClient;
+        private SignInButton googleSignInButton;
+
+        private ConnectionResult connectonResult;
+
+        private bool intentInProgress;
+        private bool signInClicked;
+        private bool infoPopulated;
+
+        TextView aa;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            aa = FindViewById<TextView>(Resource.Id.textView1);
+            aa.Text = "Maruœ kocham Cie. 555";
+
+            Button button = FindViewById<Button>(Resource.Id.StartButton);
+
+            button.Click += delegate
+            {
+                aa.Text = "Maro i Pysio zakochana paraaa. ;D";
+            };
+
+            googleSignInButton = FindViewById<SignInButton>(Resource.Id.SignInButton);
+
+            googleSignInButton.Click += googleSignInButton_Click;
+
+            GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this);
+            builder.AddConnectionCallbacks(this);
+            builder.AddOnConnectionFailedListener(this);
+            builder.AddApi(PlusClass.API);
+            builder.AddScope(PlusClass.ScopePlusProfile);
+            builder.AddScope(PlusClass.ScopePlusLogin);
+
+            googleApiClient = builder.Build();
+        }
+
+
+        private void googleSignInButton_Click(object sender, EventArgs e)
+        {
+            if (!googleApiClient.IsConnecting)
+            {
+                signInClicked = true;
+                ResolveSignInError();
+            }
+        }
+
+        private void ResolveSignInError()
+        {
+            if (googleApiClient.IsConnected)
+            {
+                return;
+            }
+
+            if (connectonResult.HasResolution)
+            {
+                try
+                {
+                    intentInProgress = true;
+                    StartIntentSenderForResult(connectonResult.Resolution.IntentSender, 0, null, 0, 0, 0);
+                }
+                catch (Android.Content.IntentSender.SendIntentException e)
+                {
+                    intentInProgress = false;
+                    googleApiClient.Connect();
+                }
+            }
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            if (requestCode == 0)
+            {
+                if (resultCode != Result.Ok)
+                {
+                    signInClicked = false;
+                }
+
+                intentInProgress = false;
+
+                if (!googleApiClient.IsConnecting)
+                {
+                    googleApiClient.Connect();
+                }
+            }
+        }
+        protected override void OnStart()
+        {
+            base.OnStart();
+            googleApiClient.Connect();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            if (googleApiClient.IsConnected)
+            {
+                googleApiClient.Disconnect();
+            }
+
+        }
+
+        public void OnConnected(Bundle connectionHint)
+        {
+            signInClicked = false;
+
+            if (infoPopulated)
+            {
+                // No need to populate info again.
+                return;
+            }
+
+            if (PlusClass.PeopleApi.GetCurrentPerson(googleApiClient) != null)
+            {
+                IPerson plusUser = PlusClass.PeopleApi.GetCurrentPerson(googleApiClient);
+
+                if (plusUser.HasDisplayName)
+                {
+                    aa.Text = plusUser.DisplayName;
+                }
+
+                // Collecting info done.
+                infoPopulated = true;
+            }
+        }
+
+        public void OnConnectionSuspended(int cause)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnConnectionFailed(ConnectionResult result)
+        {
+            if (!intentInProgress)
+            {
+                connectonResult = result;
+
+                if (signInClicked)
+                {
+                    ResolveSignInError();
+                }
+            }
+        }
     }
 }
--- WordGame/MainActivity.cs
+++ WordGame/MainActivity.cs
@@ -1,31 +1,16 @@
 ﻿using Android.App;
 using Android.Widget;
-using System;
 
-using Android.Content;
-using Android.Gms.Common;
-using Android.Gms.Common.Apis;
-using Android.Gms.Plus;
-using Android.Gms.Plus.Model.People;
-using Android.Runtime;
 using Android.OS;
 
-using static Android.Gms.Common.Apis.GoogleApiClient;
+
 
 namespace WordGame
 {
     [Activity(Label = "WordGame", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity, IConnectionCallbacks, IOnConnectionFailedListener
+    public class MainActivity : Activity
     {
 
-        private GoogleApiClient googleApiClient;
-        private SignInButton googleSignInButton;
-
-        private ConnectionResult connectonResult;
-
-        private bool intentInProgress;
-        private bool signInClicked;
-        private bool infoPopulated;
 
         TextView aa;
 
@@ -44,127 +29,6 @@ namespace WordGame
             {
                 aa.Text = "Maro i Pysio zakochana paraaa. ;D";
             };
-
-            googleSignInButton = FindViewById<SignInButton>(Resource.Id.SignInButton);
-
-            googleSignInButton.Click += googleSignInButton_Click;
-
-            GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this);
-            builder.AddConnectionCallbacks(this);
-            builder.AddOnConnectionFailedListener(this);
-            builder.AddApi(PlusClass.API);
-            builder.AddScope(PlusClass.ScopePlusProfile);
-            builder.AddScope(PlusClass.ScopePlusLogin);
-
-            googleApiClient = builder.Build();
-        }
-
-        
-        private void googleSignInButton_Click(object sender, EventArgs e)
-        {
-            if(!googleApiClient.IsConnecting)
-            {
-                signInClicked = true;
-                ResolveSignInError();
-            }
-        }
-
-        private void ResolveSignInError()
-        {
-            if(googleApiClient.IsConnected)
-            {
-                return;
-            }
-
-            if (connectonResult.HasResolution)
-            {
-                try
-                {
-                    intentInProgress = true;
-                    StartIntentSenderForResult(connectonResult.Resolution.IntentSender, 0, null, 0, 0, 0);
-                }
-                catch (Android.Content.IntentSender.SendIntentException e)
-                {
-                    intentInProgress = false;
-                    googleApiClient.Connect();
-                }
-            }
-        }
-
-        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
-        {
-            if(requestCode == 0)
-            {
-                if (resultCode != Result.Ok)
-                {
-                    signInClicked = false;
-                }
-
-                intentInProgress = false;
-
-                if(!googleApiClient.IsConnecting)
-                {
-                    googleApiClient.Connect();
-                }
-            }
-        }
-        protected override void OnStart()
-        {
-            base.OnStart();
-            googleApiClient.Connect();
-        }
-
-        protected override void OnStop()
-        {
-            base.OnStop();
-            if(googleApiClient.IsConnected)
-            {
-                googleApiClient.Disconnect();
-            }
-
-        }
-
-        public void OnConnected(Bundle connectionHint)
-        {
-            signInClicked = false;
-
-            if(infoPopulated)
-            {
-                // No need to populate info again.
-                return;
-            }
-
-            if(PlusClass.PeopleApi.GetCurrentPerson(googleApiClient) != null)
-            {
-                IPerson plusUser = PlusClass.PeopleApi.GetCurrentPerson(googleApiClient);
-
-                if (plusUser.HasDisplayName)
-                {
-                    aa.Text = plusUser.DisplayName;
-                }
-
-                // Collecting info done.
-                infoPopulated = true;
-            }
-        }
-
-        public void OnConnectionSuspended(int cause)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnConnectionFailed(ConnectionResult result)
-        {
-            if(!intentInProgress)
-            {
-                connectonResult = result;
-
-                if(signInClicked)
-                {
-                    ResolveSignInError();
-                }
-            }
-        }
     }
 }
 
